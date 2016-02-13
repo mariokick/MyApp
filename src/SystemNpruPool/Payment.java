b04ab4e8ed9a938b2403bfa49168de36207347cb
mariokick <mariokick@windowslive.com>
2016-02-13 19:36:19 +0700

    r
    r

--- src/SystemNpruPool/Payment.java
+++ src/SystemNpruPool/Payment.java
@@ -5,10 +5,57 @@
  */
 package SystemNpruPool;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Godonlyknows
  */
 public class Payment {
-    
+    public void InsertPaymet(int money,  String Datepay ,
+           String time,
+           String type,
+           int uid,
+           int stid
+          ){
+       
+		Connection connect = null;
+                Statement stmt = null;
+		
+		try {
+                        Class.forName("com.mysql.jdbc.Driver");
+                        String urlConnection = "jdbc:mysql://127.0.0.1/npru_pool?useUnicode=true&characterEncoding=UTF-8";
+                        connect = DriverManager.getConnection ( urlConnection, "root", "" );
+			stmt = connect.createStatement();
+			String sql = "INSERT INTO payment " +
+					"(P_Money,P_Date,P_Time,P_Type,U_Id,St_Id) " + 
+					"VALUES ('" + money+ "','" + Datepay + "'"
+                                + "" +
+					",'" + time + "','" + type + "','" + uid + "','" + stid + "') ";
+                         stmt.execute(sql);
+             
+                         
+             
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// Close
+		try {
+			if(connect != null){
+				stmt.close();
+				connect.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
 }
+
